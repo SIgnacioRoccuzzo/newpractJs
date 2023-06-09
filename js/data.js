26853b354f1a03d4e221a22ad44a7940bc5ddbbf
SIgnacioRoccuzzo <129063696+SIgnacioRoccuzzo@users.noreply.github.com>
2023-06-09 21:32:48 +0200

    lista de tareas

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const listaTareas = [
+    {
+        'id': 1,
+        'titulo': 'estudiar Javascript',
+        'prioridad': 'urgente',
+    },
+    {
+        'id': 2,
+        'titulo': 'dormir',
+        'prioridad': 'diario',
+    },
+    {
+        'id': 3,
+        'titulo': 'salir a comer',
+        'prioridad': 'mensual'
+    }
+]
